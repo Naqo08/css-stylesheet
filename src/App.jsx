9dd1e21ed3040877fc275c766fd3d3a6dc0602a8
Naqiuddin Kamil <naqiu923@gmail.com>
2023-08-09 14:03:27 +0800

    add stylesheet

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import HelloReact from './components/HelloReact';
+
+function App() {
+  return (
+    <>
+      <HelloReact />
+      <p className='title'>Hello React</p>
+      <p className='title'>Hello React</p>
+      <p className='title'>Hello React</p>
+      <p className='title'>Hello React</p>
+      <p className='title'>Hello React</p>
+      <p className='title'>Hello React</p>
+      <p className='title'>Hello React</p>
+
+    </>
+  );
+  
+}
+
+export default App
